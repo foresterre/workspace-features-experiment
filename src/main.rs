ef61773e28bfaa50703be67c7c30545145992d9e
Martijn Gribnau <garm@ilumeo.com>
2020-05-09 04:47:18 +0200

    Fixed oopsie; but hey, it works without nightly, huh?

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use crate_one::a_callable_fn;
 fn main() {
     a_callable_fn();
 
-    if cfg!(feature = "feat_one") {
-        println!("[crate: root] (feature = feat_one) ENABLED");
+    if cfg!(feature = "feat-one") {
+        println!("[crate: root] (feature = feat-one) ENABLED");
     } else {
-        println!("[crate: root] (feature = feat_one) DISABLED");
+        println!("[crate: root] (feature = feat-one) DISABLED");
     }
 }
